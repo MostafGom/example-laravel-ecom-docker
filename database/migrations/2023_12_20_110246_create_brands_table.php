a6d222cc685379e78598d9359941b44683e73bbd
mostafagomaa <mostafagomaa@githubgeneral.com>
2024-01-12 17:07:43 +0200

    edit migration: add unique constraint on slug

--- database/migrations/2023_12_20_110246_create_brands_table.php
+++ database/migrations/2023_12_20_110246_create_brands_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('brands', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('slug');
+            $table->string('slug')->unique();
             $table->tinyInteger('is_active')->default(1);
             $table->string('image_id')->nullable();
             $table->timestamps();
--- database/migrations/2023_12_23_125158_create_products_table.php
+++ database/migrations/2023_12_23_125158_create_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('slug');
+            $table->string('slug')->unique();
             $table->string('short_description', 255);
             $table->text('long_description');
             $table->decimal('price');
